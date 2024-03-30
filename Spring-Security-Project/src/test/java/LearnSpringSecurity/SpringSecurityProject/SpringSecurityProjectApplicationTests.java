ffdf27a5d46757599fba8827aef1fcd7264a5df6
kumarsanjeetmishra8000@gmail.com <kumarsanjeetmishra8000@gmail.com>
2024-03-30 17:37:22 +0530

    master-sprinng-security-project.

---
+++ Spring-Security-Project/src/test/java/LearnSpringSecurity/SpringSecurityProject/SpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package LearnSpringSecurity.SpringSecurityProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
